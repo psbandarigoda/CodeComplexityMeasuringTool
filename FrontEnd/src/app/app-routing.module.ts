7fcace34a1b2cd1b8ea06a10a0bc41444889d9fc
pasindu <bgpsandaruwan@gmail.com>
2020-02-29 22:33:40 +0530

    add new component

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainComponent} from "./main/main.component";
+import {UploadComponent} from "./upload/upload.component";
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'main',
+    component: MainComponent,
+    children: [
+      {
+        path: 'upload',
+        component: UploadComponent
+      }
+    ]
+  },
+
+  {path: '', pathMatch: "full", redirectTo: '/main/upload'}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,
